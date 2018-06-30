0a417d7127a7e868f12ed5860c430a6e677a56f2
masutokw <masutokw@users.noreply.github.com>
2018-07-01 00:23:02 +0200

    Step interval driven by timer2

--- tb6621.c
+++ tb6621.c
@@ -18,15 +18,18 @@ long int backslash;
 double step_size;
 unsigned int speed;
 long int position;
-long int delayt=5000;
+long int delayt=50;
 long int target;
 long int backcounter;
+uint16_t compare_time2;
+uint16_t new_time2;
+unsigned int ticks_x;
 //enum motor_state state;
 int pcounter;
 
 short resolution;
 
-
+void move_mstep(void);
 static void usart_setup(void)
 {
     /* Setup USART2 parameters. */
@@ -58,14 +61,96 @@ static void clock_setup(void)
     rcc_periph_clock_enable(RCC_USART1);
 
 }
+static void tim_setup(void)
+{
+    /* Enable TIM2 clock. */
+    rcc_periph_clock_enable(RCC_TIM2);
 
+    /* Enable TIM2 interrupt. */
+    nvic_enable_irq(NVIC_TIM2_IRQ);
+
+    /* Reset TIM2 peripheral. */
+    timer_reset(TIM2);
+
+    /* Timer global mode:
+     * - No divider
+     * - Alignment edge
+     * - Direction up
+     */
+    timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT,
+                   TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+
+    /* Reset prescaler value. */
+    timer_set_prescaler(TIM2, 24);
+
+    /* Enable preload. */
+    timer_disable_preload(TIM2);
+
+    /* Continous mode. */
+    timer_continuous_mode(TIM2);
+
+
+    timer_set_period(TIM2, 65535);
+
+    /* Disable outputs. */
+    timer_disable_oc_output(TIM2, TIM_OC1);
+    timer_disable_oc_output(TIM2, TIM_OC2);
+    timer_disable_oc_output(TIM2, TIM_OC3);
+    timer_disable_oc_output(TIM2, TIM_OC4);
+
+    /* -- OC1 configuration -- */
+
+    /* Configure global mode of line 1. */
+    timer_disable_oc_clear(TIM2, TIM_OC1);
+    timer_disable_oc_preload(TIM2, TIM_OC1);
+    timer_set_oc_slow_mode(TIM2, TIM_OC1);
+    timer_set_oc_mode(TIM2, TIM_OC1, TIM_OCM_FROZEN);
+
+    /* Set the capture compare value for OC1. */
+    timer_set_oc_value(TIM2, TIM_OC1, 1000);
+
+    /* ---- */
+
+    /* ARR reload enable. */
+    timer_disable_preload(TIM2);
+
+    /* Counter enable. */
+    timer_enable_counter(TIM2);
+
+    /* Enable commutation interrupt. */
+    timer_enable_irq(TIM2, TIM_DIER_CC1IE);
+}
+
+void tim2_isr(void)
+{
+    if (timer_get_flag(TIM2, TIM_SR_CC1IF))
+    {
+
+        /* Clear compare interrupt flag. */
+        timer_clear_flag(TIM2, TIM_SR_CC1IF);
+
+        /*
+         * Get current timer value to calculate next
+         * compare register value.
+         */
+        compare_time2 = timer_get_counter(TIM2);
+
+        /* Calculate and set the next compare value. */
+        new_time2 = compare_time2 + ticks_x;
+        timer_set_oc_value(TIM2, TIM_OC1, new_time2);
+        move_mstep();
+
+
+
+    }
+}
 static void tim3_setup(void)
 {
     /* Clock division and mode */
     TIM3_CR1 = TIM_CR1_CKD_CK_INT | TIM_CR1_CMS_EDGE;
     /* Period */
     //TIM3_ARR = 65535;
-    TIM3_ARR = 512;
+    TIM3_ARR = 255;
     /* Prescaler */
     TIM3_PSC = 0;
     TIM3_EGR = TIM_EGR_UG;
@@ -242,7 +327,7 @@ void serialParse(uint8_t* cad)
 
         break;
     case 'd':
-        delayt=atol((char*)cad);
+        ticks_x=atol((char*)cad);
 
         break;
 
@@ -272,6 +357,7 @@ int main(void)
     gpio_setup();
     usart_setup();
     parser=&serialParse;
+    tim_setup();
     tim3_setup();
     resolution=1;
     gpio_set(GPIOA,GPIO2);
@@ -279,13 +365,14 @@ int main(void)
     gpio_set(GPIOA,GPIO3);
     gpio_clear(GPIOA,GPIO1);
     gpio_clear(GPIOA,GPIO4);
+    ticks_x=1000;
 
     /* Blink the LED (PD12) on the board with every transmitted byte. */
     while (1)
     {
 
 
-        move_mstep();
+     //   move_mstep();
         for (i = 0; i < delayt; i++)  	/* Wait a bit. */
         {
             __asm__("NOP");
